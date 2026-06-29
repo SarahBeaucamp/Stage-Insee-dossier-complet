308f6d8065baf7c779fba659ef039861d431097e
SarahBeaucamp <shbeaucamp@gmail.com>
2026-06-29 10:23:50 +0000

    Structure

---
+++ Evolution et structure de la population/POP_T4/Pop_t4_R.R
@@ -0,0 +1,4 @@
+# Population par sexe et âge regroupé en 2023
+# Même tranches d'âges que pour la section tranches âge. 
+# But : à partir d'une région géographique choisie on a la répartition de la population par sexe et par tranche d'âge 
+# Là je vais juste faire le niveau commune puis ensuite une adaptation en shiny 
