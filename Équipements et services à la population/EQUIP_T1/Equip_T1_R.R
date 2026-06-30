aed377eadf84d3a4a9d02701626ffe0d3f18f31c
SarahBeaucamp <shbeaucamp@gmail.com>
2026-06-30 15:37:06 +0000

    equip_T1

---
+++ Équipements et services à la population/EQUIP_T1/Equip_T1_R.R
@@ -0,0 +1,107 @@
+# Nombre d'enfants âgés de moins de 25 ans 
+
+library(DBI)
+library(duckdb)
+library(dplyr)
+
+# 1. Créer ou ouvrir une base DuckDB locale (elle sera stockée dans un fichier 'base.duckdb')
+con <- dbConnect(duckdb(), dbdir = "base.duckdb")
+
+# 2. Charger les extensions nécessaires pour lire le S3 dans DuckDB
+dbExecute(con, "INSTALL httpfs;")
+dbExecute(con, "LOAD httpfs;")
+dbExecute(con, "INSTALL aws;")
+dbExecute(con, "LOAD aws;")
+
+# 3. Injecter automatiquement les identifiants SSP Cloud dans DuckDB
+# Le SSP Cloud expose les clés via des variables d'environnement, on les transmet à DuckDB
+dbExecute(con, "CREATE OR REPLACE SECRET (
+TYPE S3,
+PROVIDER CREDENTIAL_CHAIN,
+ENDPOINT 'minio.lab.sspcloud.fr',
+URL_STYLE 'path'
+);")
+
+# 4. Définir l'adresse de votre fichier sur le S3 du SSP Cloud
+chemin_s3 <- "s3://sarahbeaucamp/dossier_complet.parquet"
+
+# Créer une table virtuelle
+dossier_complet <- tbl(con, paste0("read_parquet('", chemin_s3, "')"))
+
+r <- dossier_complet %>%
+  filter(GEO == "44109", ID_TAB == "EQUIP_T1") %>%
+  distinct(TAB_MEASURE_LABEL) %>%
+  collect()
+
+View(r)
+
+equip <- dossier_complet %>%
+  filter(
+    TIME_PERIOD == "2024",
+    GEO_OBJECT_LABEL == "Commune",
+    GEO == "44109",
+    ID_TAB == "EQUIP_T1",
+    TAB_MEASURE_LABEL %in% c(
+      "Nombre d'équipements – Station-service",
+      "Nombre d'équipements – Station de recharge de véhicules électriques",
+      "Nombre d'équipements – Supermarché et magasin multi-commerce",
+      "Nombre d'équipements – Boulangerie-pâtisserie",
+      "Nombre d'équipements – Supérette",
+      "Nombre d'équipements – Épicerie",
+      "Nombre d'équipements – Hypermarché et grand magasin",
+      "Nombre d'équipements – Coiffure"
+    )
+  ) %>%
+  
+  mutate (
+    équipements = case_when(
+      TAB_MEASURE_LABEL == "Nombre d'équipements – Station-service" ~ "Station-service",
+      TAB_MEASURE_LABEL == "Nombre d'équipements – Station de recharge de véhicules électriques" ~ "Station de recharge de véhicules électriques",
+      TAB_MEASURE_LABEL == "Nombre d'équipements – Boulangerie-pâtisserie" ~ "Boulangerie-pâtisserie",
+      TAB_MEASURE_LABEL %in% c("Nombre d'équipements – Supérette", "Nombre d'équipements – Épicerie") ~ "Epicerie, Supérette",
+      TAB_MEASURE_LABEL %in% c("Nombre d'équipements – Hypermarché et grand magasin", "Nombre d'équipements – Supermarché et magasin multi-commerce") ~ "Grande Surface",
+      TAB_MEASURE_LABEL == "Nombre d'équipements – Coiffure" ~ "Coiffure"
+    )
+  ) %>%
+  
+  group_by(équipements) %>%
+  summarise(total_groupe = sum(OBS_VALUE, na.rm = TRUE)) %>%
+  
+  collect() %>%
+  
+  mutate(
+    total = sum(total_groupe),
+    part_groupe = (total_groupe / total) * 100
+  )
+
+View(equip)
+
+# Histogramme 
+
+library(ggplot2)
+library(stringr)
+
+ggplot(equip, aes(x = équipements, y = total_groupe)) +
+  geom_col(fill = "steelblue") + 
+  
+  #coord_flip() + 
+  
+  scale_x_discrete(
+    limits = rev(levels(equip$équipements)),
+    labels = function(x) str_wrap(x, width = 10) 
+  ) +
+  
+  scale_y_continuous(expand = expansion(mult = c(0, 0.1))) +
+  
+  labs(
+    title = "Nombre de commerces en 2024", #2022 n'existe pas pour ces données
+    x = "",
+    y = ""
+  ) +
+  
+  theme_minimal() +
+  theme(
+    plot.title = element_text(face = "bold", size = 14),
+    axis.title = element_text(face = "bold"),
+    axis.text.y = element_text(lineheight = 0.9) 
+  )
